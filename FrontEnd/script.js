2cf4a070be82d71a391f1b1e8288170bf61eda09
Victor Chraibi <victor.chraibi@gmail.com>
2024-11-22 14:57:14 +0100

    Filter buttons JS, Login HTML&CSS

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,19 +1,51 @@
+// Wait for HTML content before fetchworks
 document.addEventListener('DOMContentLoaded', () => {
     fetchWorks();
 });
 
-function fetchWorks() {
-    fetch('http://localhost:5678/api/works')
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            displayWorks(data);
-        });
+// Fetch Backend Data
+async function fetchWorks() {
+    try {
+        const response = await fetch('http://localhost:5678/api/works');
+        const works = await response.json();
+        createFilters(works);
+        displayWorks(works);
+    } catch (error) {
+        console.error('Error fetching works:', error);
+    }
 }
 
+// Dynamic Filter button creation
+function createFilters(works) {
+    const filtersContainer = document.querySelector('#filters');
+    const categories = [
+        { id: 'all', name: 'Tous' },
+        { id: 1, name: 'Objets' },
+        { id: 2, name: 'Appartements' },
+        { id: 3, name: 'Hotels & Restaurants' }
+    ];
+
+    // Button creation for each category
+    categories.forEach(category => {
+        const button = document.createElement('button');
+        button.textContent = category.name;
+        button.dataset.id = category.id;
+        button.addEventListener('click', () => {
+            if (category.id === 'all') {
+                displayWorks(works);
+            } else {
+                const filteredWorks = works.filter(work => work.categoryId === category.id);
+                displayWorks(filteredWorks);
+            }
+        });
+        filtersContainer.appendChild(button);
+    });
+}
+
+// Displaying works
 function displayWorks(works) {
-    const gallery = document.querySelector('.gallery'); 
+    const gallery = document.querySelector('.gallery');
+    gallery.innerHTML = '';
 
     works.forEach(work => {
         const workItem = document.createElement('figure');
